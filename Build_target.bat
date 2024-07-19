550c8c91c9eda3a5bdecc1c5793aba15f7ca3d3b
gengyou <resisttempetation23@gmail.com>
2024-07-19 13:24:47 +0800

    fix: Build_target.bat

--- Build_target.bat
+++ Build_target.bat
@@ -1,2 +1,12 @@
+set "SOURCE_DIR=C:\GengYouFutures\CppTester"
+set "BUILD_DIR=C:\GengYouFutures\CppTester\build"
+
 cmake -S "%SOURCE_DIR%" -B "%BUILD_DIR%"
+
+REM 切換到構建目錄
+cd /d "%BUILD_DIR%"
+
+REM 使用 CMake 和 Visual Studio 進行構建
+echo Building project...
+cmake --build . --config Debug
 
