7a8718be780ef3833412bc1c7e7d9c73e7a301cb
gengyou <resisttempetation23@gmail.com>
2024-07-21 18:12:35 +0800

    imp: Logger.cpp add release mode disable debug message

--- Logger/Logger.cpp
+++ Logger/Logger.cpp
@@ -1,26 +1,37 @@
 #include "Logger.h"
 #include <iostream>
 
+// 在 Release 版本中禁用调试信息
+#ifndef NDEBUG
+#define LOGGING_ENABLED
+#endif
+
 Logger::Logger(const std::string &filename) : logFile(filename, std::ios::out | std::ios::app)
 {
+#ifdef LOGGING_ENABLED
     if (!logFile.is_open())
     {
         std::cerr << "Failed to open log file: " << filename << std::endl;
     }
+#endif
 }
 
 Logger::~Logger()
 {
+#ifdef LOGGING_ENABLED
     if (logFile.is_open())
     {
         logFile.close();
     }
+#endif
 }
 
 void Logger::log(const std::string &message, const std::string &functionName)
 {
+#ifdef LOGGING_ENABLED
     if (logFile.is_open())
     {
         logFile << "[" << functionName << "] " << message << std::endl;
     }
+#endif
 }
