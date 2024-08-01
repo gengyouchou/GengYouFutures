10992450ef32c54ceab217fc543a5094d03cc278
gengyouchou <resisttempetation23@gmail.com>
2024-08-02 02:35:05 +0800

    chore: change build target path

--- Build_target.bat
+++ Build_target.bat
@@ -4,7 +4,7 @@ REM Build_target.bat
 REM 自动检测当前盘符
 
 set "SOURCE_DIR=%cd%\CppTester"
-set "BUILD_DIR=%cd%\CppTester\CppTester\x64\Release"
+set "BUILD_DIR=%cd%\CppTester\x64"
 
 REM 确保 CMakeLists.txt 文件存在于指定目录
 if exist "%SOURCE_DIR%\CMakeLists.txt" (
