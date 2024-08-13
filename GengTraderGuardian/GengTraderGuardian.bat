aecd72c0a4ad3329ed36d0fd97b694586896dc8a
gengyouchou <resisttempetation23@gmail.com>
2024-08-14 01:23:40 +0800

    imp: add GengTraderGuardian\GengTraderGuardian.bat

--- GengTraderGuardian/GengTraderGuardian.bat
+++ GengTraderGuardian/GengTraderGuardian.bat
@@ -3,22 +3,24 @@ setlocal
 
 :: 设置变量
 set "processName=CppTester.exe"
+set "checkInterval=10"  :: 检查间隔时间（秒）
+
+:: 获取当前目录
 set "currentDir=%~dp0"
-set "processPath=%currentDir%\%processName%"
 
 :: 获取当前驱动器
 for %%d in ("%currentDir%") do set "driveLetter=%%d"
 
-:: 检测驱动器是否存在
-if exist "%driveLetter%\" (
-    :: 设置进程路径
-    set "processPath=%driveLetter%\GengYouFutures\CppTester\x64\Debug\%processName%"
+:: 设置进程路径
+set "processPath=%driveLetter%\GengYouFutures\CppTester\x64\Debug\%processName%"
 
+:: 每 10 秒检查进程是否运行
+:checkLoop
     :: 检查进程是否正在运行
-    tasklist /FI "IMAGENAME eq %processName%" 2>NUL | find /I /N "%processName%">NUL
+    tasklist /FI "IMAGENAME eq %processName%" 2>NUL | find /I /N "%processName%" >NUL
     if "%ERRORLEVEL%"=="0" (
         :: 进程正在运行
-        echo %processName% is already running.
+        echo %processName% is running.
     ) else (
         :: 进程没有运行，尝试启动
         echo %processName% is not running. Restarting...
@@ -33,8 +35,9 @@ if exist "%driveLetter%\" (
             echo The path %processPath% does not exist.
         )
     )
-) else (
-    echo Drive %driveLetter% does not exist.
-)
+
+    :: 等待 10 秒后再次检查
+    timeout /t %checkInterval% /nobreak >NUL
+    goto checkLoop
 
 endlocal
