30d82ae11fbe1c91a3048391e554b16aa5868b3f
gengyou <resisttempetation23@gmail.com>
2024-08-17 13:19:10 +0800

    fix: imp: CountBidOfferLongShort using totalBid * 3 <= totalOffer * 2, and impove gBest5BidOffer add current close and Qty

--- CppTester/CppTester/Strategy.cpp
+++ CppTester/CppTester/Strategy.cpp
@@ -586,22 +586,21 @@ LONG CountBidOfferLongShort(LONG nStockidx)
     if (totalBid * 3 <= totalOffer * 2)
     {
         ++countLong;
-    }
 
-    if (nClose > 0 && nClose <= gBest5BidOffer[nStockidx][0].first && nQty >= 10)
-    {
-        --countLong;
+        if (nClose > 0 && nClose <= gBest5BidOffer[nStockidx][0].first && nQty >= 10)
+        {
+            --countLong;
+        }
     }
 
     if (totalOffer * 3 <= totalBid * 2)
     {
-
         --countShort;
-    }
 
-    if (nClose > 0 && nClose >= gBest5BidOffer[nStockidx][5].first && nQty >= 10)
-    {
-        ++countShort;
+        if (nClose > 0 && nClose >= gBest5BidOffer[nStockidx][5].first && nQty >= 10)
+        {
+            ++countShort;
+        }
     }
 
     LOG(DEBUG_LEVEL_DEBUG, "countLong = %ld, countShort=%ld", countLong, countShort);
