6a4944b02f255fda781ec9d8af6ee08f278724be
gengyou <resisttempetation23@gmail.com>
2024-09-07 19:48:05 +0800

    imp: add StrategySimpleNewLongShortPosition

--- CppTester/CppTester/Strategy.cpp
+++ CppTester/CppTester/Strategy.cpp
@@ -1977,6 +1977,136 @@ VOID StrategyNewCostAverageBiasAutoLongShortPosition(string strUserId, LONG MtxC
     DEBUG(DEBUG_LEVEL_DEBUG, "End");
 }
 
+/**
+ * @brief Implements a futures trading strategy based on the relationship between the cost line and the moving average line.
+ *
+ * Strategy Overview:
+ *
+ * 1. Cost Line Above the Moving Average Line:
+ *    - Bearish Bias: Short the market when the price falls below the cost line, with the moving average line trending downward.
+ *    - If the price rebounds and breaks through the moving average line, followed by a breakthrough of the cost line, switch to a bullish bias.
+ *
+ * 2. Cost Line Below the Moving Average Line:
+ *    - Bullish Bias: Go long when the price breaks above the cost line, with the moving average line trending upward.
+ *    - If the price declines, breaking below the moving average line, and continues to fall below the cost line, switch to a bearish bias.
+ *
+ * 3. Cost Line Equals the Moving Average Line:
+ *    - Range-Bound Market: The market is considered range-bound, with the price oscillating between the cost line and the moving average line.
+ *    - If the distance between the two lines is less than 20 points, refrain from trading.
+ *
+ * @param costLine The cost line value.
+ * @param movingAverageLine The moving average line value.
+ * @param currentPrice The current market price.
+ * @return The trading action to be taken (e.g., buy, sell, hold).
+ */
+
+VOID StrategySimpleNewLongShortPosition(string strUserId, LONG MtxCommodtyInfo, LONG LongShort)
+{
+    DEBUG(DEBUG_LEVEL_DEBUG, "Start");
+
+    if (gOpenInterestInfo.NeedToUpdate == TRUE)
+    {
+        // Only new positions need to be checked
+        LOG(DEBUG_LEVEL_DEBUG, "gOpenInterestInfo.NeedToUpdate == TRUE");
+        return;
+    }
+
+    double OpenPrice = 0;
+
+    if (gCurCommHighLowPoint.count(MtxCommodtyInfo) == 0)
+    {
+        return;
+    }
+    else
+    {
+        OpenPrice = static_cast<double>(gCurCommHighLowPoint[MtxCommodtyInfo][2]) / 100.0;
+    }
+
+    double curPrice = 0;
+
+    if (gCurCommPrice.count(MtxCommodtyInfo) != 0)
+    {
+        curPrice = static_cast<double>(gCurCommPrice[MtxCommodtyInfo]) / 100.0;
+    }
+
+    double CurAvg = 0;
+    double CurAmp = 0;
+
+    double CurHigh = 0, CurLow = 0;
+
+    if (gCurCommHighLowPoint.count(MtxCommodtyInfo) > 0)
+    {
+        CurHigh = gCurCommHighLowPoint[MtxCommodtyInfo][0] / 100.0;
+        CurLow = gCurCommHighLowPoint[MtxCommodtyInfo][1] / 100.0;
+        CurAmp = CurHigh - CurLow;
+        CurAvg = (CurHigh + CurLow) / 2;
+    }
+
+    DEBUG(DEBUG_LEVEL_DEBUG, "curPrice = %f, CurAvg= %f, gCostMovingAverageVal=%f",
+          curPrice, CurAvg, gCostMovingAverageVal);
+
+    if (CurAmp > EstimatedTodaysAmplitude())
+    {
+        return;
+    }
+
+    // Do Long
+
+    if (LongShort == 1 && gOpenInterestInfo.openPosition <= 0)
+    {
+        vector<string> vec = {COMMODITY_OTHER};
+
+        for (auto &x : vec)
+        {
+            AutoOrder(x,
+                      ORDER_NEW_POSITION,     // New
+                      ORDER_BUY_LONG_POSITION // Buy or sell
+            );
+        }
+
+        // Greedy assumptions always have positions
+
+        {
+            gOpenInterestInfo.product = COMMODITY_OTHER;
+            gOpenInterestInfo.buySell = "B";
+            gOpenInterestInfo.openPosition += 1;
+            gOpenInterestInfo.avgCost = curPrice;
+        }
+
+        LOG(DEBUG_LEVEL_INFO, "New Long position, curPrice = %f, gCostMovingAverageVal= %f, CurAvg= %f, StrategyCaluLongShort: %ld",
+            curPrice, gCostMovingAverageVal, CurAvg, StrategyCaluLongShort());
+    }
+
+    // Do Short
+
+    if (LongShort == 0 && gOpenInterestInfo.openPosition >= 0)
+    {
+        vector<string> vec = {COMMODITY_OTHER};
+
+        for (auto &x : vec)
+        {
+            AutoOrder(x,
+                      ORDER_NEW_POSITION,       // New
+                      ORDER_SELL_SHORT_POSITION // Buy or sell
+            );
+        }
+
+        // Greedy assumptions always have positions
+
+        {
+            gOpenInterestInfo.product = COMMODITY_OTHER;
+            gOpenInterestInfo.buySell = "S";
+            gOpenInterestInfo.openPosition -= 1;
+            gOpenInterestInfo.avgCost = curPrice;
+        }
+
+        LOG(DEBUG_LEVEL_INFO, "New Short position, curPrice = %f, gCostMovingAverageVal= %f, CurAvg= %f, StrategyCaluLongShort: %ld",
+            curPrice, gCostMovingAverageVal, CurAvg, StrategyCaluLongShort());
+    }
+
+    DEBUG(DEBUG_LEVEL_DEBUG, "End");
+}
+
 VOID StrategySwitch(IN LONG Mode, IN LONG MtxCommodtyInfo)
 {
     if (!(gCurServerTime[0] <= 5 || gCurServerTime[0] >= 15) &&
