54071007f1e5f8383a82d707e5d1981e7271fbfc
gengyou <resisttempetation23@gmail.com>
2024-09-08 17:15:38 +0800

    fix: build waring

--- CppTester/cmdCaptureServer/socketServer.cpp
+++ CppTester/cmdCaptureServer/socketServer.cpp
@@ -16,18 +16,17 @@
 
 #include "socketServer.h"
 
-
 #pragma comment(lib, "Ws2_32.lib")
 
-char buffer[10240]  = {0};
-char buffer2[10240] ={0};
+char buffer[10240] = {0};
+char buffer2[10240] = {0};
 extern std::deque<long> gDaysKlineDiff;
 extern std::unordered_map<long, std::array<long, 4>> gCurCommHighLowPoint;
 extern SHORT gCurServerTime[3];
 extern std::unordered_map<long, long> gCurCommPrice;
 extern std::unordered_map<SHORT, std::array<long, 4>> gCurTaiexInfo;
 extern std::unordered_map<long, vector<pair<long, long>>> gBest5BidOffer;
-extern std::unordered_map<long, std::array<long, 5>> gTransactionList;
+extern std::unordered_map<long, std::array<long, 6>> gTransactionList;
 
 extern COMMODITY_INFO gCommodtyInfo;
 extern DAY_AMP_AND_KEY_PRICE gDayAmpAndKeyPrice;
@@ -37,10 +36,8 @@ extern LONG gTransactionListLongShort;
 extern double gCostMovingAverageVal;
 extern STRATEGY_CONFIG gStrategyConfig;
 
-
 extern string g_strUserId;
 extern string gPwd;
-
 
 void thread_socket()
 {
@@ -48,38 +45,38 @@ void thread_socket()
     SOCKET server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    
+
     const char *greeting = "Connected to server";
 
     // 初始化 Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
         std::cerr << "WSAStartup failed" << std::endl;
-      
     }
 
     // 创建套接字文件描述符
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    {
         std::cerr << "Socket creation failed" << std::endl;
         WSACleanup();
-        
     }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
     // 绑定套接字到端口
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == SOCKET_ERROR) {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == SOCKET_ERROR)
+    {
         std::cerr << "Bind failed" << std::endl;
         closesocket(server_fd);
         WSACleanup();
-       
     }
     // 监听传入连接
-    if (listen(server_fd, 3) == SOCKET_ERROR) {
+    if (listen(server_fd, 3) == SOCKET_ERROR)
+    {
         std::cerr << "Listen failed" << std::endl;
         closesocket(server_fd);
         WSACleanup();
-       
     }
     std::cout << "Server is listening on port " << PORT << std::endl;
 
@@ -88,35 +85,51 @@ void thread_socket()
     // strncpy(tab2, temp.c_str(), sizeof(tab2));
     // tab2[sizeof(tab2) - 1] = 0;
 
-    char buffer_empty[4096]={0};
+    char buffer_empty[4096] = {0};
 
-    while (true) {
+    while (true)
+    {
         std::cout << "Waiting for new connection..." << std::endl;
         // 接受客户端连接
         new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
-        if (new_socket == INVALID_SOCKET) {
+        if (new_socket == INVALID_SOCKET)
+        {
             std::cerr << "Accept failed" << std::endl;
             continue; // 继续等待其他客户端连接
         }
 
-        send(new_socket, greeting, strlen(greeting), 0); // 发送初始连接消息
+        size_t length = strlen(greeting);
+        if (length > INT_MAX)
+        {
+            // 處理錯誤，例如打印錯誤消息或截斷數據
+        }
+        else
+        {
+            send(new_socket, greeting, static_cast<int>(length), 0); // 发送初始连接消息
+        }
 
-        while (true) {
-            std::cout<<"client in";
+        while (true)
+        {
+            std::cout << "client in";
             // Sleep(500);
             // int valread = 1023;
             int valread = recv(new_socket, buffer_empty, 4096, 0); // 读取客户端消息
-            if (valread > 0) {
+            if (valread > 0)
+            {
 
                 buffer_empty[valread] = '\0'; // 确保字符串以 null 结尾
                 std::cout << "Message from client: " << buffer_empty << std::endl;
 
                 send(new_socket, buffer2, 4095, 0); // 发送消息回客户端
                 // send(new_socket, tab2, 1023, 0); // 发送消息回客户端
-            } else if (valread == 0) {
+            }
+            else if (valread == 0)
+            {
                 std::cout << "Client disconnected" << std::endl;
                 break; // 客户端断开连接，退出内循环
-            } else {
+            }
+            else
+            {
                 std::cerr << "recv failed" << std::endl;
                 break;
             }
