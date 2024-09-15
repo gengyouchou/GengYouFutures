0263048d22ca4af7b78edeea46072b71517e992f
gengyouchou <resisttempetation23@gmail.com>
2024-09-16 00:38:19 +0800

    imp: add killer.bat for ssh control

---
+++ GengTraderGuardian/ViewDebugLog.bat
@@ -0,0 +1,7 @@
+for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
+	set year=%datetime:~0,4%
+	set month=%datetime:~4,2%
+	set day=%datetime:~6,2%
+	set formattedDate=%year%%month%%day%
+	set debugLog=%formattedDate%_debug.log
+	vim %debugLog%
