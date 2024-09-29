d7528b41d203df5834c500f2546084fe08f4631e
gengyou <resisttempetation23@gmail.com>
2024-09-29 20:02:55 +0800

    chore: clean up code for SKOSQuoteLib

--- CppTester/CppTester/SKOSQuoteLib.h
+++ CppTester/CppTester/SKOSQuoteLib.h
@@ -38,23 +38,13 @@ public:
     long RequestTicks(short *psPageNo, string strStockNos);
     long RequestStockList(short MarketNo);
     long GetStockByIndexLONG(long bStockIndex, SKCOMLib::SKFOREIGNLONG *pSKStock);
-    long RequestKLine(string strStockNo);
-    long RequestServerTime();
     long RequestStockIndexMap(IN string strStockNo, OUT SKCOMLib::SKFOREIGNLONG *pSKStock);
-    long GetMarketBuySellUpDown(VOID);
-    void ProcessDaysOrNightCommHighLowPoint();
     VOID GetCommodityIdx(VOID);
 
     // Events
     void OnConnection(long nKind, long nCode);
     void OnNotifyQuoteLONG(short sMarketNo, long nStockIndex);
     void OnNotifyTicksNineDigitLONG(LONG nStockIndex, LONG nPtr, LONG nDate, LONG lTimehms, LONG nClose, LONG nQty);
-
-    void OnNotifyStockList(long sMarketNo, string strStockData);
-    void OnNotifyKLineData(BSTR bstrStockNo, BSTR bstrData);
-    void OnNotifyServerTime(SHORT sHour, SHORT sMinute, SHORT sSecond, LONG nTotal);
-    void OnNotifyMarketTot(SHORT sMarketNo, SHORT sPtr, LONG nTime, LONG nTotv, LONG nTots, LONG nTotc);
-    void OnNotifyMarketBuySell(SHORT sMarketNo, SHORT sPtr, LONG nTime, LONG nBc, LONG nSc, LONG nBs, LONG nSs);
 
 private:
     HRESULT OnEventFiringObjectInvoke(
@@ -71,10 +61,3 @@ private:
     SKCOMLib::ISKOSQuoteLibPtr m_pSKOSQuoteLib;
     ISKOSQuoteLibEventHandler *m_pSKOSQuoteLibEventHandler;
 };
-
-VOID GetCommodityIdx(VOID);
-void loadHighLowPoints();
-void updateHighLowPoints(const std::string &date, double dayHigh, double dayLow, double nightHigh, double nightLow);
-
-extern std::unordered_map<long, std::array<long, 6>> gTransactionList;
-// long nPtr, long nBid, long nAsk, long nClose, long nQty
