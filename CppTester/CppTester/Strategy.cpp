d7dd7f2152c9a13325245b4b804474d983160441
gengyouchou <resisttempetation23@gmail.com>
2024-10-26 00:57:07 +0800

    fix: Avoid the impact caused by the time difference between the first or last spot transaction

--- CppTester/CppTester/Strategy.cpp
+++ CppTester/CppTester/Strategy.cpp
@@ -1070,7 +1070,9 @@ LONG AutoOrder(IN string ProductNum, IN SHORT NewClose, IN SHORT BuySell)
     DEBUG(DEBUG_LEVEL_DEBUG, "Started");
 
     if ((gCurServerTime[0] == 13 && gCurServerTime[1] == 30) ||
-        (gCurServerTime[0] == 21 && gCurServerTime[1] == 30))
+        (gCurServerTime[0] == 21 && gCurServerTime[1] == 30) ||
+        (gCurServerTime[0] == 9 && gCurServerTime[1] == 0) ||
+        (gCurServerTime[0] == 5 && gCurServerTime[1] == 0))
     {
         return 0;
     }
