68b03eec82f72b07baca0e26127129399452a4cb
gengyouchou <resisttempetation23@gmail.com>
2024-11-25 23:13:19 +0800

    imp: UpdateLongShortIntegralValue at specific time

--- CppTester/CppTester/main.cpp
+++ CppTester/CppTester/main.cpp
@@ -395,6 +395,12 @@ void thread_main()
 
         StrategySwitch(gStrategyConfig.StrategyMode, MtxCommodtyInfo);
 
+        if (gCurServerTime[0] == 5 && gCurServerTime[1] == 0 && gCurServerTime[2] == 0 ||
+            gCurServerTime[0] == 13 && gCurServerTime[1] == 45 && gCurServerTime[2] == 0)
+        {
+            UpdateLongShortIntegralValue(gLongShort);
+        }
+
         // Ouput start
 
         if (elapsed.count() >= refreshInterval)
