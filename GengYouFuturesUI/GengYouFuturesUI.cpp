6d1f8514b8640e39e54134bb429ffac94da24c33
gengyou <resisttempetation23@gmail.com>
2024-12-01 20:05:03 +0800

    imp: Simulated price output

--- GengYouFuturesUI/GengYouFuturesUI.cpp
+++ GengYouFuturesUI/GengYouFuturesUI.cpp
@@ -2,12 +2,25 @@
 #include <winhttp.h>
 #include <iostream>
 #include <string>
-#include <thread> // 用於加入延遲
+#include <thread>  // 用於加入延遲
+#include <cstdlib> // 用於 rand() 和 srand()
+#include <ctime>   // 用於 time()
 
 #pragma comment(lib, "winhttp.lib")
 
+// 生成隨機價格波動
+double generateRandomPrice(double currentPrice)
+{
+    // 設定波動範圍：±10 元
+    double fluctuation = (rand() % 21 - 10) / 10.0; // 隨機值介於 -1.0 到 1.0 之間
+    return currentPrice + fluctuation;
+}
+
 int main()
 {
+    // 初始化隨機數生成器
+    srand(static_cast<unsigned int>(time(0)));
+
     // 伺服器資訊
     LPCWSTR serverName = L"localhost";
     INTERNET_PORT serverPort = 8080;
@@ -34,8 +47,13 @@ int main()
         return 1;
     }
 
+    double currentPrice = 1234.56; // 初始期貨價格
+
     while (true) // 無窮迴圈
     {
+        // 生成隨機價格
+        currentPrice = generateRandomPrice(currentPrice);
+
         // 建立請求
         HINTERNET hRequest = WinHttpOpenRequest(hConnect, L"POST", endpoint,
                                                 NULL, WINHTTP_NO_REFERER,
@@ -47,8 +65,9 @@ int main()
             break;
         }
 
-        // 模擬發送的資料
-        std::string jsonData = R"({"time": "2024-11-25 10:05", "price": 1234.56, "volume": 100, "symbol": "FUTURE1"})";
+        // 模擬發送的資料，隨機波動價格
+        std::string jsonData = R"({"time": "2024-11-25 10:05", "price": )" + std::to_string(currentPrice) +
+                               R"(, "volume": 100, "symbol": "FUTURE1"})";
 
         // 設置標頭
         BOOL result = WinHttpAddRequestHeaders(hRequest, L"Content-Type: application/json", -1L, WINHTTP_ADDREQ_FLAG_ADD);
