ad38b77eaa8cc3f8cefbe416561deee704c7b769
unknown <adslppp200224575@gmail.com>
2024-12-05 00:19:25 +0800

    imp: using cpp-httplib

--- GengYouFuturesUI/GengYouFuturesUI.cpp
+++ GengYouFuturesUI/GengYouFuturesUI.cpp
@@ -1,105 +1,39 @@
-#include <windows.h>
-#include <winhttp.h>
+#include "httplib.h"
 #include <iostream>
 #include <string>
-#include <thread>
 #include <cstdlib>
 #include <ctime>
+#include <nlohmann/json.hpp> // 用於生成 JSON
 
-#pragma comment(lib, "winhttp.lib")
-
+using json = nlohmann::json;
 double generateRandomPrice(double currentPrice)
 {
     double fluctuation = (rand() % 21 - 10) / 10.0;
     return currentPrice + fluctuation;
 }
 
-bool sendData(HINTERNET hConnect, LPCWSTR endpoint, const std::string &jsonData)
-{
-    HINTERNET hRequest = WinHttpOpenRequest(hConnect, L"POST", endpoint,
-                                            NULL, WINHTTP_NO_REFERER,
-                                            WINHTTP_DEFAULT_ACCEPT_TYPES,
-                                            WINHTTP_FLAG_REFRESH);
-    if (!hRequest)
-    {
-        std::cerr << "WinHttpOpenRequest failed, error: " << GetLastError() << std::endl;
-        return false;
-    }
-
-    BOOL result = WinHttpAddRequestHeaders(hRequest, L"Content-Type: application/json", -1L, WINHTTP_ADDREQ_FLAG_ADD);
-    if (!result)
-    {
-        std::cerr << "Failed to set request headers, error: " << GetLastError() << std::endl;
-    }
-
-    result = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0,
-                                (LPVOID)jsonData.c_str(), (DWORD)jsonData.size(),
-                                (DWORD)jsonData.size(), 0);
-    if (!result)
-    {
-        std::cerr << "WinHttpSendRequest failed, error: " << GetLastError() << std::endl;
-        WinHttpCloseHandle(hRequest);
-        return false;
-    }
-
-    result = WinHttpReceiveResponse(hRequest, NULL);
-    if (result)
-    {
-        std::cout << "Data sent successfully: " << jsonData << std::endl;
-    }
-    else
-    {
-        std::cerr << "WinHttpReceiveResponse failed, error: " << GetLastError() << std::endl;
-    }
-
-    WinHttpCloseHandle(hRequest);
-    return result;
-}
-
 int main()
 {
     srand(static_cast<unsigned int>(time(0)));
-
-    LPCWSTR serverName = L"localhost";
-    INTERNET_PORT serverPort = 8080;
-    LPCWSTR endpoint = L"/futures";
-
-    HINTERNET hSession = WinHttpOpen(L"Futures Client/1.0",
-                                     WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
-                                     WINHTTP_NO_PROXY_NAME,
-                                     WINHTTP_NO_PROXY_BYPASS, 0);
-
-    if (!hSession)
-    {
-        std::cerr << "WinHttpOpen failed, error: " << GetLastError() << std::endl;
-        return 1;
-    }
-
-    HINTERNET hConnect = WinHttpConnect(hSession, serverName, serverPort, 0);
-    if (!hConnect)
-    {
-        std::cerr << "WinHttpConnect failed, error: " << GetLastError() << std::endl;
-        WinHttpCloseHandle(hSession);
-        return 1;
-    }
+    httplib::Server svr;
 
     double currentPrice = 20000.0;
-    while (true)
-    {
+
+    svr.Get("/", [&currentPrice](const httplib::Request &, httplib::Response &res)
+            {
         currentPrice += generateRandomPrice(1);
+        json data = {
+            {"time", time(0)},
+            {"price", currentPrice},
+            {"volume", 100},
+            {"symbol", "FUTURE1"}};
+        res.set_content(data.dump(), "application/json"); });
 
-        std::string jsonData = R"({"time": ")" + std::to_string(time(0)) + R"(", "price": )" +
-                               std::to_string(currentPrice) + R"(, "volume": 100, "symbol": "FUTURE1"})";
+    svr.set_error_handler([](const httplib::Request &req, httplib::Response &res)
+                          {
+        res.set_content(R"({"error": "Unsupported method"})", "application/json");
+        res.status = 501; });
 
-        if (!sendData(hConnect, endpoint, jsonData))
-        {
-            break;
-        }
-
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-    }
-
-    WinHttpCloseHandle(hConnect);
-    WinHttpCloseHandle(hSession);
-    return 0;
+    std::cout << "Server started at http://localhost:8000" << std::endl;
+    svr.listen("0.0.0.0", 8000);
 }
