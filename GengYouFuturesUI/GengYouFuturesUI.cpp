4481d8b7674b0686f25fee90f976fa917db86b6c
GengYouZhou <GengYouZhou@ami.com>
2024-12-06 18:51:54 +0800

    imp: add sub path for http get

--- GengYouFuturesUI/GengYouFuturesUI.cpp
+++ GengYouFuturesUI/GengYouFuturesUI.cpp
@@ -5,7 +5,12 @@
 #include <ctime>
 #include <nlohmann/json.hpp>
 #include <thread>
+#include <deque>
+#include <string>
+#include <vector>
 
+#include <iostream>
+#include <sstream>
 using json = nlohmann::json;
 
 // 模擬外部全域變數
@@ -16,6 +21,70 @@ long gCurCommPrice[10] = {0}, gEvaluatePosition = 1;
 double gFutureRight = 10000, gClosedProfitLoss = 1234;
 long gDayAmpAndKeyPrice_LongKey1 = 0, gDayAmpAndKeyPrice_SmallestAmp = 100;
 double gBidOfferLongShortSlope = 1, gNumberOfStocksRisingAndFalling = 1;
+
+// bid offer data
+
+std::unordered_map<long, std::array<long, 4>> gCurCommHighLowPoint; // {High, Low, Open, Data}
+std::unordered_map<long, vector<pair<long, long>>> gBest5BidOffer;
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+json AutoBest5LongToJson(LONG ProductIdxNo, const std::string &ProductName)
+{
+    json responseData;
+
+    if (gCurCommHighLowPoint.count(ProductIdxNo) > 0)
+    {
+        long CurHigh = gCurCommHighLowPoint[ProductIdxNo][0];
+        long CurLow = gCurCommHighLowPoint[ProductIdxNo][1];
+        long Open = gCurCommHighLowPoint[ProductIdxNo][2];
+
+        responseData["ProductName"] = ProductName;
+        responseData["CurrentPrice"] = gCurCommPrice[ProductIdxNo];
+        responseData["High"] = CurHigh;
+        responseData["Low"] = CurLow;
+        responseData["Open"] = Open;
+    }
+
+    if (gBest5BidOffer.count(ProductIdxNo) && gBest5BidOffer[ProductIdxNo].size() >= 10)
+    {
+        long TotalBid = gBest5BidOffer[ProductIdxNo][0].second +
+                        gBest5BidOffer[ProductIdxNo][1].second +
+                        gBest5BidOffer[ProductIdxNo][2].second +
+                        gBest5BidOffer[ProductIdxNo][3].second +
+                        gBest5BidOffer[ProductIdxNo][4].second;
+        long TotalOffer = gBest5BidOffer[ProductIdxNo][9].second +
+                          gBest5BidOffer[ProductIdxNo][8].second +
+                          gBest5BidOffer[ProductIdxNo][7].second +
+                          gBest5BidOffer[ProductIdxNo][6].second +
+                          gBest5BidOffer[ProductIdxNo][5].second;
+
+        json bidOfferData;
+        for (int i = 0; i < 5; ++i)
+        {
+            bidOfferData["Bid" + std::to_string(i + 1)] = {
+                {"Price", gBest5BidOffer[ProductIdxNo][i].first},
+                {"Volume", gBest5BidOffer[ProductIdxNo][i].second}};
+        }
+        for (int i = 5; i < 10; ++i)
+        {
+            bidOfferData["Offer" + std::to_string(i - 4)] = {
+                {"Price", gBest5BidOffer[ProductIdxNo][i].first},
+                {"Volume", gBest5BidOffer[ProductIdxNo][i].second}};
+        }
+
+        responseData["BidOffer"] = bidOfferData;
+        responseData["TotalBid"] = TotalBid;
+        responseData["TotalOffer"] = TotalOffer;
+    }
+    else
+    {
+        responseData["Error"] = "Insufficient bid-offer data.";
+    }
+
+    return responseData;
+}
 
 // 模擬生成隨機價格
 double generateRandomPrice(double currentPrice)
@@ -63,24 +132,15 @@ void startHttpServer(double &currentPrice)
         std::cout << "Sending index response: " << indexData.dump(4) << std::endl;
         res.set_content(indexData.dump(), "application/json"); });
 
-    // 處理 BidOffer.html 的數據
-    svr.Get("/bid-offer-data", [&currentPrice](const httplib::Request &req, httplib::Response &res)
+    svr.Get("/bid-offer-data", [](const httplib::Request &req, httplib::Response &res)
             {
-        currentPrice += generateRandomPrice(1);
+    LONG ProductIdxNo = 0; // 假設為測試的產品索引
+    std::string ProductName = "FUTURE1";
 
-        json bidOfferData = {
-            {"UserId", g_strUserId},
-            {"BidPrice", currentPrice - 5.0},
-            {"OfferPrice", currentPrice + 5.0},
-            {"BidVolume", rand() % 100 + 1},
-            {"OfferVolume", rand() % 100 + 1},
-            {"BidOfferSlope", gBidOfferLongShortSlope},
-            {"NumberOfStocksRisingAndFalling", gNumberOfStocksRisingAndFalling},
-            {"Time", time(0)},
-            {"Symbol", "FUTURE1"}};
+    json bidOfferData = AutoBest5LongToJson(ProductIdxNo, ProductName);
 
-        std::cout << "Sending bid-offer response: " << bidOfferData.dump(4) << std::endl;
-        res.set_content(bidOfferData.dump(), "application/json"); });
+    std::cout << "Sending bid-offer response: " << bidOfferData.dump(4) << std::endl;
+    res.set_content(bidOfferData.dump(), "application/json"); });
 
     // 錯誤處理，若方法不支持則回傳 501
     svr.set_error_handler([](const httplib::Request &req, httplib::Response &res)
