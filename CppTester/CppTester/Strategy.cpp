8354539d8e3a376f8ca59625fa5b54df84d194d9
GengYou Zhou (周庚佑) <GengYouZhou@ami.com>
2024-12-17 01:02:12 +0800

    imp: cancel night trade StrategyClosePositionOnDayTrade

--- CppTester/CppTester/Strategy.cpp
+++ CppTester/CppTester/Strategy.cpp
@@ -3333,7 +3333,6 @@ VOID StrategySwitch(IN LONG Mode, IN LONG MtxCommodtyInfo)
         StrategyTakeFuturesProfit(g_strUserId, MtxCommodtyInfo);
         StrategyClosePosition(g_strUserId, MtxCommodtyInfo);
         bool TimeIsUp = StrategyClosePositionOnDayTrade(g_strUserId, MtxCommodtyInfo, 13, 40);
-        TimeIsUp = TimeIsUp | StrategyClosePositionOnDayTrade(g_strUserId, MtxCommodtyInfo, 04, 40);
 
         if (TimeIsUp == TRUE)
         {
@@ -3411,7 +3410,6 @@ VOID StrategySwitch(IN LONG Mode, IN LONG MtxCommodtyInfo)
         StrategyTakeFuturesProfit(g_strUserId, MtxCommodtyInfo);
         StrategyClosePosition(g_strUserId, MtxCommodtyInfo);
         bool TimeIsUp = StrategyClosePositionOnDayTrade(g_strUserId, MtxCommodtyInfo, 13, 29);
-        TimeIsUp = TimeIsUp | StrategyClosePositionOnDayTrade(g_strUserId, MtxCommodtyInfo, 04, 40);
 
         if (TimeIsUp == TRUE)
         {
